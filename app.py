fd29e9ab92693e571e7d3997dcde31b8b9dafcaa
Zachary Garcia <garcia.zacharyw@gmail.com>
2020-07-16 22:50:36 -0700

    minor updates and data refresh

--- app.py
+++ app.py
@@ -15,11 +15,13 @@ server = app.server
 # NYT: https://github.com/nytimes/covid-19-data/blob/master/us-counties.csv
 
 zips_and_fips = pd.read_csv('zip-to-fips.csv')
-og_data = pd.read_csv('nyt-covid-7-14.csv')
+og_data = pd.read_csv('nyt-covid-data.csv')
 # Note that ~30% of counties are missing from NYT covid data!
+
+# Add new column to df with <county name> County, <state name>
 og_data['fullname'] = og_data['county'] + ' County, ' + og_data['state']
 
-# create list of county and state names for input dropdown
+# create list of county and state name dicts for input dropdown
 all_fullnames = []
 for name in og_data['fullname'].unique():
     dict_entry = {'label': name, 'value': name}
@@ -27,6 +29,7 @@ for name in og_data['fullname'].unique():
 
 
 def get_county_data_from_name(fullname):
+    """using fullname from input, pull data from og_data and return a new df with date, cases, deaths for that county"""
     my_data = og_data[og_data.fullname == fullname]
     
     county_name = my_data['county'].unique()
@@ -40,24 +43,19 @@ def get_county_data_from_name(fullname):
     return my_filtered_data
 
 
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
+def get_more_details_from_name(fullname):
+    """get date of most recent entry, number of cases as of that date"""
+    my_data = og_data[og_data.fullname == fullname]
+
+    most_recent_date = my_data['date'].max()
+#     deaths = my_data[my_data.date == most_recent_date]['deaths'].item()
+    cases = my_data[my_data.date == most_recent_date]['cases'].item()
+
+    return(most_recent_date, cases)
+
 
 app.layout = html.Div(children=[
-    html.H2(children='COVID-19 cases reported to date'),
-
-    # html.Div(children='''
-    #     Enter your county.
-    # '''),
+    html.H5(children='Choose your county'),
 
     dcc.Dropdown(
         id='input',
@@ -67,12 +65,11 @@ app.layout = html.Div(children=[
 
     html.Div(id='output-graph'),
 
-    html.Div(children='''
-        Data courtesy of The New York Times, based on reports from state and local health agencies. 
-    '''),
+    html.Div(id='last-updated'),
 
-    dcc.Link('NYT Covid tracking page', href='https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html.')
-])
+    html.Div('Data courtesy of The New York Times, based on reports from state and local health agencies.')
+
+ ])
 
 @app.callback(
     Output(component_id='output-graph', component_property='children'),
@@ -81,8 +78,6 @@ app.layout = html.Div(children=[
 def update_value(input_data):
     
     result = get_county_data_from_name(input_data)
-    # county_name = result['county']
-    # county_data = result['data'] 
 
     return dcc.Graph(
         id='example-graph',
@@ -91,15 +86,28 @@ def update_value(input_data):
                 {
                     'x': result.date,
                     'y': result.cases,
-                    'type': 'line',
-                    'name': input_data
+                    'type': 'bar',
+                    'name': 'Covid-19 cases in ' + input_data
                 },
             ],
             'layout': {
-                'title': input_data
+                'title': 'Covid-19 cases in ' + input_data
             }
         }
     )
+
+@app.callback(
+    Output(component_id='last-updated', component_property='children'),
+    [Input(component_id='input', component_property='value')]
+)
+def update_other_values(input_data):
+
+    most_recent_date, total_cases = get_more_details_from_name(input_data)
+
+    text = 'last updated ' + most_recent_date 
+
+    return text
+
 
 
 if __name__ == '__main__':
